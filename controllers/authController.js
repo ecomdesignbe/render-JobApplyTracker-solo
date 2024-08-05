635762647fd68c9394c0c4d71b1239c8d6664daa
Steve Vandenbossche <90630250+ecomdesignbe@users.noreply.github.com>
2024-08-05 16:30:52 +0200

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -189,8 +189,25 @@ module.exports.createJob_post = async (req, res) => {
 }
 
 /************************************************************** */
+// debugger
+module.exports.viewJob_get = async (req, res) => {
+  try {
+    const { id } = req.params;
+    console.log('Received ID:', id);
+    const data = await Job.findById(id);
+    console.log('Fetched Data:', data);
+    if (!data) {
+      return res.status(404).json({ message: "Job not found" });
+    }
+    res.render('viewjob', { data });
+  } catch (error) {
+    console.error('Error occurred:', error);
+    res.status(500).json({ message: error.message });
+  }
+}
 
-module.exports.viewJob_get = async (req, res, next) => {
+/*
+module.exports.viewJob_get = async (req, res) => {
   try {
     const { id } = req.params
     const data = await Job.findById(id)
@@ -199,10 +216,10 @@ module.exports.viewJob_get = async (req, res, next) => {
     }
     res.render('viewjob', { data }) 
   } catch (error) {
-    return next(err)
-    // res.status(500).json({ message: error.message })
+    res.status(500).json({ message: error.message })
   }
-}
+};
+*/
 
 /************************************************************** */
 
