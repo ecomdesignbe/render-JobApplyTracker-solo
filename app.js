5575d427ace924d99cfa7164220f840a69de0da1
Steve Vandenbossche <90630250+ecomdesignbe@users.noreply.github.com>
2024-08-05 16:04:05 +0200

    add path modules

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const authRoutes = require('./routes/authRoutes')
 const cookieParser = require('cookie-parser')
 const { requireAuth, checkUser } = require('./middleware/authMiddleware')
+const path = require('path')
 
 const app = express()
 
@@ -13,6 +14,7 @@ app.use(cookieParser())
 
 // view engine
 app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views')); 
 
 
 // database connection
