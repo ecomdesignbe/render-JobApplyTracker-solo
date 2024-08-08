e08e758ca6ef133f16847cb08a72616c1bd6e33a
Steve Vandenbossche <90630250+ecomdesignbe@users.noreply.github.com>
2024-08-08 16:10:53 +0200

    Update app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 // view engine
-app.set('views', path.join('__dirname, views'))
+app.set('views, __dirname + ./views');
 app.set('view engine', 'ejs')
 
 
