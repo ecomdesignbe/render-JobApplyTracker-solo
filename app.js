c6cafaa43689dab6b5c36937d83b5045557571fd
Steve Vandenbossche <90630250+ecomdesignbe@users.noreply.github.com>
2024-08-09 12:32:28 +0200

    Update app.js

--- app.js
+++ app.js
@@ -32,15 +32,12 @@ mongoose.connect(dbURI)
 app.get('*', checkUser)
 app.get('/', (req, res) => res.render('login'))
 app.get('/profile', (req, res) => res.render('profile'))
-// app.get('/viewjob/:id')
+app.get('/viewjob/:id')
 app.get('/editjob/:id')
 app.get('/delitejob/:id')
 app.get('/dashboard')
 app.get('/register', redirectIfLoggedIn, (req, res) => res.render('register'))
-app.get('/viewjob/:id', async (req, res) => {
-  const data = await Job.findById(id)
-  res.render('viewjob', { data });
-});
+
 
 
 
