39c064e8fb90c06f9bd681d43e36051075fd6b96
Steve Vandenbossche <90630250+ecomdesignbe@users.noreply.github.com>
2024-08-09 12:10:07 +0200

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const FTPClient = require('ftp')
 const app = express()
 
 // middleware
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json())
 app.use(cookieParser())
 
